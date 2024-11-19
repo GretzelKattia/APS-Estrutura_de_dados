97f5afd6560442c06c59521f894726d28d45fa35
Katchau <gretzel.penaloza@aluno.unip.br>
2024-11-18 21:03:14 -0300

    Update imagem.h

--- imagem.h
+++ imagem.h
@@ -4,8 +4,8 @@
 typedef struct {
     int codigo_alerta;
     float area;
-    char data_inicio[11];
-    char data_fim[11];
+    char data_foto_antes[11];
+    char data_foto_depois[11];
     char estado[3];
     char municipio[50];
 } Imagem;
