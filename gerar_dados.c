ea582bb2db20f28655be556cf7cf67562d71608c
Katchau <gretzel.penaloza@aluno.unip.br>
2024-11-18 21:02:18 -0300

    Update gerar_dados.c

--- gerar_dados.c
+++ gerar_dados.c
@@ -11,14 +11,26 @@ void gerar_dados_internos(Imagem imagens[], int n) {
         "Sinop", "Vilhena", "Humaita"
     };
     int num_estados = 9;
-    int num_municipios = 9;
+    int num_municipios = 20;
 
     for (int i = 0; i < n; i++) {
         imagens[i].codigo_alerta = rand() % 1000000;  // Gera valores aleatórios entre 0 e 999999
         imagens[i].area = ((float)rand() / (float)(RAND_MAX)) * 100.0;  // Área aleatória entre 0 e 100
-        snprintf(imagens[i].data_inicio, 11, "%02d/%02d/%04d", rand() % 30 + 1, rand() % 12 + 1, 2000 + rand() % 23);  // Datas aleatórias entre 2000 e 2022
-        snprintf(imagens[i].data_fim, 11, "%02d/%02d/%04d", rand() % 30 + 1, rand() % 12 + 1, 2000 + rand() % 23);
+        snprintf(imagens[i].data_foto_antes, 11, "%02d/%02d/%04d", rand() % 30 + 1, rand() % 12 + 1, 2000 + rand() % 23);  // Datas aleatórias entre 2000 e 2022
+        snprintf(imagens[i].data_foto_depois, 11, "%02d/%02d/%04d", rand() % 30 + 1, rand() % 12 + 1, 2000 + rand() % 23);
         snprintf(imagens[i].estado, 3, "%s", estados[rand() % num_estados]);
         snprintf(imagens[i].municipio, 50, "%s", municipios[rand() % num_municipios]);
     }
+}
+
+void salvar_dados_aleatorios(const Imagem imagens[], int n, const char* filename) {
+    FILE *arquivo = fopen(filename, "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        exit(1);
+    }
+    for (int i = 0; i < n; i++) {
+        fprintf(arquivo, "%d,%.2f,%s,%s,%s,%s\n", imagens[i].codigo_alerta, imagens[i].area, imagens[i].data_foto_antes, imagens[i].data_foto_depois, imagens[i].estado, imagens[i].municipio);
+    }
+    fclose(arquivo);
 }
